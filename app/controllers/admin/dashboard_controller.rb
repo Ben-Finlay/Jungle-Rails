02eb65face86286b35cb9fb08be0cb3b4e43a8bb
Ben Finlay <benjamin.finlay@gmail.com>
2022-06-01 14:19:16 -0400

    Add content for the Admin Dashboard.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -6,5 +6,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: name, password: password
 
   def show
+    @total_products = Product.count
+    @total_categories = Category.count
   end
 end
